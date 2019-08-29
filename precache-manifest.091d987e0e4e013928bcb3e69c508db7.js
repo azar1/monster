1035579ab765cc5a0e3ea7594d4aa467ab44be57
azar1 <azarpathan05@gmmail.com>
2019-08-29 19:12:09 +0530

    Updates

---
+++ precache-manifest.091d987e0e4e013928bcb3e69c508db7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc15c7fce3c8dd908c7a6282493fdac",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "6d6c1a5cf4090878c013",
+    "url": "/monster/static/css/main.3ed6c050.chunk.css"
+  },
+  {
+    "revision": "8ba932ef6ae70f2ce43d",
+    "url": "/monster/static/js/2.2d5fb830.chunk.js"
+  },
+  {
+    "revision": "6d6c1a5cf4090878c013",
+    "url": "/monster/static/js/main.0e517978.chunk.js"
+  },
+  {
+    "revision": "8feb12953517aaa79ef7",
+    "url": "/monster/static/js/runtime~main.0d0f241b.js"
+  }
+]);
